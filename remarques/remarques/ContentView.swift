efc9330b45225849fefefbecb161d38c35dca010
kevalpipalia <keval.pipalia@dialogue.co>
2024-04-20 21:40:51 -0400

    Initialize xcode project

---
+++ remarques/remarques/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  remarques
+//
+//  Created by Keval Pipalia on 2024-04-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
